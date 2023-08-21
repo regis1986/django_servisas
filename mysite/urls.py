2247d1035cf3a82ce719370a5e2effd0a8d53f0a
Regis <r.petrauskui@gmail.com>
2023-08-21 16:09:29 +0300

    pabandymas su endpointo pavadinimu

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-path('autoservisas', include('autoservisas.urls')),
+path('qq', include('autoservisas.urls')),
     path('admin/', admin.site.urls),
 ]
